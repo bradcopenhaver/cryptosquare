6d7bcca961afb9db0dfab6c3d94b0816263e6fc7
Brad Copenhaver and Luke Philips <brad@>
2016-10-27 14:56:24 -0700

    clean up code and rename some variables

--- js/scripts.js
+++ js/scripts.js
@@ -8,47 +8,36 @@ $(document).ready(function() {
 });
 var inputArray = [];
 var gridArray = [];
+var encryptedArray =[];
 var outputArray= [];
+
 var encoder = function(input) {
-var lowerString = input.toLowerCase();
-  for (i=0;i<lowerString.length; i++) {
-
-    if (/[a-z0-9]/.test(lowerString.charAt(i))) {
-    inputArray.push(lowerString[i]);
-    }
-  };
-
-  var subArrayLength = gridSize(inputArray.length);
-  for (var i=0; i< inputArray.length; i += subArrayLength) {
-    gridArray.push(inputArray.slice(i , i + subArrayLength));
+for (i=0;i<input.length; i++) {
+  if (/[a-z0-9]/i.test(input.charAt(i))) {
+  inputArray.push((input[i]).toLowerCase());
   }
+};
 
-  for (i=0; i<subArrayLength; i++) {
-    for (j=0; j < gridArray.length; j++) {
-      outputArray.push(gridArray[j][i]);
-    }
+var subArrayLength = gridSize(inputArray.length);
+for (var i=0; i< inputArray.length; i += subArrayLength) {
+  gridArray.push(inputArray.slice(i , i + subArrayLength));
+}
+
+for (i=0; i<subArrayLength; i++) {
+  for (j=0; j < gridArray.length; j++) {
+    encryptedArray.push(gridArray[j][i]);
   }
-  var encryptedString = outputArray.join("");
-  var encryptedArray =[];
-  for (i=0; i < encryptedString.length; i+=5) {
-    encryptedArray.push(encryptedString.slice(i, i+5));
-    console.log(encryptedArray);
-  }
+}
+var encryptedString = encryptedArray.join("");
 
-
-  return encryptedArray.join(" ");
+for (i=0; i < encryptedString.length; i+=5) {
+  outputArray.push(encryptedString.slice(i, i+5));
+}
+return outputArray.join(" ");
 };
 
 var gridSize = function(length) {
   var squareRootN = Math.sqrt(length);
-  var sideA = Math.ceil(squareRootN);
-  var sideB;
-
-  if ( sideA * sideA - length > sideA ) {
-    sideB = sideA - 1;
-  }
-  else {
-    sideB = sideA;
-  }
-  return sideA;
+  var sideLength = Math.ceil(squareRootN);
+  return sideLength;
 };
